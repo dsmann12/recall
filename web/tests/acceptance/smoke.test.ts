79a8b952be7348288fe40b5f76b1eb3b4b16a788
David Scheuermann <dsmann1212@gmail.com>
2025-10-21 21:30:27 -0500

    test: update playwright to set test server and base url
    update smoke tests to make use of base url by using relative paths.

--- web/tests/acceptance/smoke.test.ts
+++ web/tests/acceptance/smoke.test.ts
@@ -1,15 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-const RECALL_CLIENT_TEST_URL = 'http://localhost:5173';
 
 test('has title', async ({ page }) => {
-  await page.goto(RECALL_CLIENT_TEST_URL);
+  await page.goto('/');
 
   await expect(page).toHaveTitle(/recall/i);
 });
 
 test('has construction placeholder', async ({ page }) => {
-  await page.goto(RECALL_CLIENT_TEST_URL);
+  await page.goto('/');
 
   await expect(page.getByText(/recall is currently under construction/i)).toBeVisible();
 });
